daf485e9d9be3d4f2114e254ace3b7aabe132ccf
ipadorusa <ipadorusa@gmail.com>
2018-03-26 17:52:08 +0900

    modify commit

--- animation/ani.js
+++ animation/ani.js
@@ -6,75 +6,97 @@ jQuery(document).ready(function(){
                 _innerbar = jQuery(".innerbar"),
                 _movingObj = jQuery('.runningman', _wrap_ani);
 
-            var startIdx = 0;
-            var startIdx2 = 0;
-            var leftPosition = 0;
-            var leftPosition2 = 0;
-            var shiftLenth = 28;
-            var totalPage = 10;
             var tl = new TimelineLite();
+            var number_graph = _movingObj.length;
+            var index_graph = 0;
+            var isArrival = [];
+            _movingObj.each(function(i) {
+                isArrival[i] = false;
+            });
 
 
-            function movingAct() {
-                _movingObj.each(function(i){
-                    tl.to(_movingObj[i], 1, {                                                
-                        delay: 0.3,
-                        left:_movingObj.eq(0).data("average") + '%',
-                        ease:Bounce,
-                        onStart: moveFnc,
-                        onComplete: finishFnc
-                    });
+            // requestAnimationFrame 폴리필
+            if(!Date.now){
+                Date.now = function(){
+                    return +new Date;
+                };
+            }
+            if(!window['performance']) window.performance = {};
+            if(!performance.now) performance.now = (function(){
+                var offset = Date.now(); //브라우저의 실행시점을 기억한다.
+                return function(){
+                    return Date.now() - offset; //경과시간을 반환한다.
+                };
+            })();
+            if(!window['requestAnimationFrame']){
+                window.requestAnimationFrame = window['webkitRequestAnimationFrame']
+                    || window['mozRequestAnimationFrame']
+                    || window['msRequestAnimationFrame']
+                    || function(func) {
+                        if(!func.__raf__){
+                            func.__raf__ = function(){
+                                func(performance.now());
+                            };
+                        }
+                        return setTimeout(func.__raf__, 1000 / 60);
+                    };
+                window.cancelAnimationFrame = window['webkitCancelAnimationFrame']
+                    || window['mozCancelAnimationFrame']
+                    || window['msCancelAnimationFrame']
+                    || clearTimeout;
+            }
+
+
+
+            function rotation() {
+                if(index_graph < number_graph) {
+                    movingAct(index_graph);
+                    index_graph++;
+                }
+            }
+            function movingAct(i) {
+                tl.to(_movingObj[i], _movingObj.eq(i).data("average") / 55, {
+                    left:_movingObj.eq(i).data("average") + '%',
+                    ease:Bounce,
+                    onStart: runningFnc(i, 0),
+                    onComplete : function() {
+                        isArrival[i] = true;
+                        rotation();
+                    }
                 });
             }
-            movingAct();
+            rotation();
 
-            // function aniStart(i) {
-            //     tl.to(
-            //         _movingObj.eq(i),
-            //          5, {
-            //             left:_movingObj.eq(i).data("average") + '%',
-            //             backgroundPosition:"-392px -37px",
-            //             ease:Bounce
-                        
-            //         }).set(_movingObj.eq(i),
-            //          {
-            //             backgroundPosition:"0 -37px"
-            //        })
-            // }
-            
-            
-            function moveFnc(i) {
-                let start = performance.now();
-                console.log('start',start)
-                totalPage = 10;
-                startIdx++;
-                if(startIdx < totalPage) {
-                    leftPosition -= shiftLenth;
-                    _movingObj.eq(0).css("background-position", leftPosition + "px -37px" );
-                    requestAnimationFrame(moveFnc,1000 / 4);
+            function runningFnc(objIndex, perc) {
+                var cur = perc + 0.3;
+                if(perc >= 11) {
+                    cur = 0
                 }
-                
-            }
+                var leftPosition = - (Math.floor(cur) * 28);
 
-            function finishFnc(i) {
-                var totalPage2 = 4;
-                console.log('bbbbbbbbbbb', totalPage2, startIdx2,leftPosition2)          
-                
-                startIdx2++;
-                if(startIdx2 < totalPage2) {                    
-                    leftPosition2 -= shiftLenth;
-                    _movingObj.eq(0).css("background-position", leftPosition2 + "px -74px" );
-                    requestAnimationFrame(finishFnc,100);
+                if(!isArrival[objIndex]){
+                    _movingObj.eq(objIndex).addClass('run');
+                    _movingObj.eq(objIndex).css("background-position", leftPosition + "px 0" );
+                    requestAnimationFrame(function () {
+                        runningFnc(objIndex, cur )
+                    });
+                } else {
+                    finishFnc(objIndex, 0);
+                    _innerbar.eq(objIndex).addClass('on');
                 }
             }
 
-            // 바그래프업
-            function barFadeUp(i) {
-                return function() {
-                    jQuery(".innerbar").eq(i).addClass('on');
+            // finish action
+            function finishFnc(objIndex, perc) {
+                var cur = perc + 0.2;
+                var leftPosition = - (Math.floor(cur) * 28);
+                if(cur < 4) {
+                    _movingObj.eq(objIndex).css("background-position", leftPosition + "px -37px" );
+                    requestAnimationFrame(function () {
+                        finishFnc(objIndex, cur)
+                    });
                 }
             }
-
         }
     };
 
