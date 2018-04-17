188b431efa8d0c97e9273be19992a88b243c940a
ipadorusa <ipadorusa@gmail.com>
2018-04-18 01:18:39 +0900

    수정

---
+++ read-link/20180418.js
@@ -0,0 +1,15 @@
+var data = [
+    {
+        'data' : '20180418',
+        'url' : 'https://codeburst.io/top-javascript-vscode-extensions-for-faster-development-c687c39596f5',
+        'category' : 'addon',
+        'title' : 'Top JavaScript VSCode Extensions for Faster Development'
+    },
+    {
+        'data' : '20180418',
+        'url' : 'https://www.json.org/json-ko.html',
+        'category' : 'json',
+        'title' : 'json개요'
+    }
+
+]
