7909d49717a040cf38556015a48e543fc7ef149f
ipadorusa <ipadorusa@gmail.com>
2018-04-19 00:17:54 +0900

    ^^

---
+++ read-link/20180419.js
@@ -0,0 +1,20 @@
+var data = [
+    {
+        "date" : "20180419",
+        "url" : "https://slides.com/sdrasner/svg-can-do-that#/",
+        "category" : "svg",
+        "title" : "SVG can do THAT?!"
+    },
+    {
+        "date" : "20180419",
+        "url" : "https://developers.google.com/web/fundamentals/design-and-ux/animations/css-vs-javascript?hl=ko",
+        "category" : "css,animations",
+        "title" : "CSS와 자바스크립트 애니메이션"
+    },
+    {
+        "date" : "20180419",
+        "url" : "https://calyfactory.github.io/%EC%A0%9C%EC%9D%B4%EC%8A%A8%ED%8C%8C%EC%8B%B1/",
+        "category" : "json",
+        "title" : "json을 파싱해보자"
+    }
+]
