232b0fa58451af8d34e538fa14db744310cb0bac
ipadorusa <ipadorusa@gmail.com>
2018-06-29 15:47:54 +0900

    'circle-2

--- d3/circle-2.js
+++ d3/circle-2.js
@@ -1,27 +1,24 @@
 (function () {
 
-    //Function to return the data for the inset rings.
-    //Probably won't need to use a funciton could just set this to a variable
     var triRadialData = [
-            { index: 0, name: 'Funded', icon: "", percentage: 60 },
-            { index: 1, name: 'Worked', icon: "", percentage: 44 }
+            { index: 0, name: 'Funded', percentage: 60 },
+            { index: 1, name: 'Worked', percentage: 44 }
     ];
 
 
-
-    //Colors array.
-    var colors = ["#938df9", "#71b5fc"];
+    var colors = ['#938df9', '#71b5fc'];
     var config = {
         innerRadius : [100, 86],
         outerRadius : [94, 61]
     };
 
     //build function that creates the visuals
-    function build(dataset, elementToBind,cfg) {
+    function build(dataset, elementToBind, cfg) {
         var container = d3.select(elementToBind),
             width = container.node().getBoundingClientRect().width,
             height = container.node().getBoundingClientRect().height,
-            tau = 2 * Math.PI;
+            tau = 2 * Math.PI,
+            formatPercent = d3.format(".0%");
 
 
 
@@ -32,13 +29,9 @@
                         return d.percentage / 100 * tau;
                     })
                     .innerRadius(function (d) {
-                        /*return innerRadius - d.index * (30 + gap)*/
-                        console.log('a-1',d.index)
                         return config.innerRadius[d.index];
                     })
                     .outerRadius(function (d) {
-                        /*return outterRadius - d.index * (30 + gap)*/
-                        console.log('a-2',d.index)
                         return config.outerRadius[d.index];
                     });
 
@@ -54,79 +47,78 @@
                             });
 
         //Append the background ring to the body
-        var svg = d3.select(elementToBind).append("svg")
-            .attr("width", '100%')
-            .attr("height", '100%')
-            .attr('viewBox', '0 0 ' + Math.min(width, height) + ' ' + Math.min(width, height))
-            .attr('preserveAspectRatio', 'xMinYMin')
-            .append("g")
-            .attr("transform", "translate(" + Math.min(width, height) / 2 + "," + Math.min(width, height) / 2 + ")");
+        var svg = d3.select(elementToBind)
+                    .append('svg')
+                    .attr('width', '100%')
+                    .attr('height', '100%')
+                    .attr('viewBox', '0 0 ' + Math.min(width, height) + ' ' + Math.min(width, height))
+                    .attr('preserveAspectRatio', 'xMinYMin')
+                    .append('g')
+                    .attr('transform', 'translate(' + Math.min(width, height) / 2 + ',' + Math.min(width, height) / 2 + ')');
 
 
 
         //enter the data set and loop through it
-        var field = svg.selectAll("g")
+        var field = svg.selectAll('g')
             .data(dataset)
-            .enter().append("g");
+            .enter().append('g');
 
         //Attach the filled in progress path
-        field.append("path").attr("class", "bg")
+        field.append('path').attr('class', 'bg')
             .attr("fill", '#e9eef4')
             .attr("d", background);
 
-        field.append("path")
-            .attr("class", "progress")
+        field.append('path')
+            .attr('class', 'progress')
             .style("fill", function (d) {
                 return colors[d.index];
             });
 
+        var description1 = field.append('text')
+            .attr('text-anchor', 'middle')
+            .attr('class', 'description_text description_text_01')
+            .attr('dx', '4px')
+            .attr('dy', '-5px');
 
+        var description2 = field.append('text')
+            .attr('text-anchor', 'middle')
+            .attr('class', 'description_text description_text_02')
+            .attr('dx', '4px')
+            .attr('dy', '25px');
 
-
-
-
-        d3.transition().duration(1750).each(update);
 
         function update() {
             field = field
-                .each(function (d) {
-                    this._value = d.percentage;
+                    .each(function (d) {
+                        this._value = d.percentage;
+                    })
+                    .data(dataset)
+                    .each(function (d) {
+                        d.previousValue = this._value;
+                    });
+            field.selectAll('path.progress')
+                .transition()
+                .duration(function(d) {
+                    return d.percentage * 25
                 })
-                .data(dataset)
-                .each(function (d) {
-                    d.previousValue = this._value;
-                });
-
-            //Animation to paint the bars one by one
-            field.selectAll("path.progress").transition().duration(750).delay(function (d, i) {
-                return i * 500
-            })
+                .delay(50)
                 .ease(d3.easeSin)
                 .attrTween("d", arcTween);
-
-            /*field.select("text.icon").text(function (d) {
-                return d.icon;
-            })
-                .attr("transform", function (d) {
-                    return "translate(5," + -((innerRadius + 7) - d.index * (30 + gap)) + ")"
-
-                });
-
-            field.select("text.completed").text(function (d) {
-                return Math.round(d.percentage / 100 * 10);
-            });*/
         }
 
         function arcTween(d) {
             var i = d3.interpolateNumber(0, d.percentage);
             return function (t) {
                 d.percentage = i(t);
+                description1.text(formatPercent(data[0].percentage / 100));
+                description2.text(formatPercent(data[1].percentage / 100));
                 return arc(d);
             };
         }
+        update();
     }
 
     //Call the build function
-    build(triRadialData, ".donut", config);
+    build(triRadialData, '.donut', config);
 
 })()
